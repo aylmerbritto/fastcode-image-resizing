403b86b3195a279e57a2eba81843aa13c5fd2807
aylmerbritto <aylmerbritto@cmu.edu>
2023-05-03 19:36:26 -0400

    wip:cleaning submission branch

--- performanceDriverNN.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-fileList=$(ls -v inputs/black-images)
-for i in $fileList;do
-    size=$(echo $i | grep -Eo '[0-9]{1,4}' | head -1)
-    ./build/performanceNN inputs/black-images/$i $size $size 
-    ./build/bmnn inputs/black-images/$i
-done
--- performanceDriverOMP.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-fileList=$(ls -v inputs/black-images)
-for i in $fileList;do
-    size=$(echo $i | grep -Eo '[0-9]{1,4}' | head -1)
-    ./build/parallel inputs/gpu-images/$i $size $size
-done
